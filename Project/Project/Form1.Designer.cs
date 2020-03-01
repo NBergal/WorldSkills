969488c8d634d00bc6db3c9e9694ebaadd8ff7a4
NBergal <57491088+NBergal@users.noreply.github.com>
2020-03-01 21:00:14 +0300

    TEST 1

---
+++ Project/Project/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.worldSkillsDataSet = new Project.WorldSkillsDataSet();
+            this.worldSkillsDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.employeeTBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.employeeTTableAdapter = new Project.WorldSkillsDataSetTableAdapters.EmployeeTTableAdapter();
+            this.txtEmployeeID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cbxPositionID = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.txtOffice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtAge = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.worldSkillsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.worldSkillsDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeTBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.txtEmployeeID,
+            this.txtName,
+            this.cbxPositionID,
+            this.txtOffice,
+            this.txtAge});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(760, 437);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView1_UserDeletingRow);
+            // 
+            // worldSkillsDataSet
+            // 
+            this.worldSkillsDataSet.DataSetName = "WorldSkillsDataSet";
+            this.worldSkillsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // worldSkillsDataSetBindingSource
+            // 
+            this.worldSkillsDataSetBindingSource.DataSource = this.worldSkillsDataSet;
+            this.worldSkillsDataSetBindingSource.Position = 0;
+            // 
+            // employeeTBindingSource
+            // 
+            this.employeeTBindingSource.DataMember = "EmployeeT";
+            this.employeeTBindingSource.DataSource = this.worldSkillsDataSetBindingSource;
+            // 
+            // employeeTTableAdapter
+            // 
+            this.employeeTTableAdapter.ClearBeforeFill = true;
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.DataPropertyName = "EmployeeID";
+            this.txtEmployeeID.HeaderText = "EmployeeID";
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Visible = false;
+            // 
+            // txtName
+            // 
+            this.txtName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.txtName.DataPropertyName = "Name";
+            this.txtName.HeaderText = "Name";
+            this.txtName.Name = "txtName";
+            // 
+            // cbxPositionID
+            // 
+            this.cbxPositionID.DataPropertyName = "PositionID";
+            this.cbxPositionID.HeaderText = "PositionID";
+            this.cbxPositionID.Name = "cbxPositionID";
+            // 
+            // txtOffice
+            // 
+            this.txtOffice.DataPropertyName = "Office";
+            this.txtOffice.HeaderText = "Office";
+            this.txtOffice.Name = "txtOffice";
+            // 
+            // txtAge
+            // 
+            this.txtAge.DataPropertyName = "Age";
+            this.txtAge.HeaderText = "Age";
+            this.txtAge.Name = "txtAge";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.worldSkillsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.worldSkillsDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeTBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource worldSkillsDataSetBindingSource;
+        private WorldSkillsDataSet worldSkillsDataSet;
+        private System.Windows.Forms.BindingSource employeeTBindingSource;
+        private WorldSkillsDataSetTableAdapters.EmployeeTTableAdapter employeeTTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtEmployeeID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtName;
+        private System.Windows.Forms.DataGridViewComboBoxColumn cbxPositionID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtOffice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtAge;
+    }
+}
+
---
+++ Project/Project/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=LAPTOP-ENHD60TK\SQLEXPRESS;Initial Catalog=WS;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridViewPositionTOutput();
+            dataGridViewEmployeeTOutput();
+        }
+
+        void dataGridViewPositionTOutput()
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDA = new SqlDataAdapter("SELECT * FROM PositionT", sqlCon);
+                DataTable dt = new DataTable();
+                sqlDA.Fill(dt);
+                cbxPositionID.ValueMember = "PositionID";
+                cbxPositionID.DisplayMember = "Position";
+                DataRow topItem = dt.NewRow();
+                topItem[0] = 0;
+                topItem[1] = "-=SELECT=-";
+                dt.Rows.InsertAt(topItem, 0);
+                cbxPositionID.DataSource = dt;
+            }
+        }
+
+        void dataGridViewEmployeeTOutput()
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDA = new SqlDataAdapter("SELECT * FROM EmployeeT", sqlCon);
+                DataTable dt = new DataTable();
+                sqlDA.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    DataGridViewRow dgvRow = dataGridView1.CurrentRow;
+                    SqlCommand sqlCmd = new SqlCommand("EmployeeAddOrEdit", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                    //INSERT
+                    if (dgvRow.Cells["txtEmployeeID"].Value == DBNull.Value)
+                    {
+                        sqlCmd.Parameters.AddWithValue("@EmployeeID", 0);
+                    }
+
+                    //UPDATE
+                    else
+                    {
+                        sqlCmd.Parameters.AddWithValue("@EmployeeID", Convert.ToInt32(dgvRow.Cells["txtEmployeeID"].Value));
+                    }
+
+                    sqlCmd.Parameters.AddWithValue("@Name", dgvRow.Cells["txtName"].Value == DBNull.Value ? "" : dgvRow.Cells["txtName"].Value.ToString());
+                    sqlCmd.Parameters.AddWithValue("@PositionID", Convert.ToInt32(dgvRow.Cells["cbxPositionID"].Value == DBNull.Value ? "1" : dgvRow.Cells["cbxPositionID"].Value.ToString()));
+                    sqlCmd.Parameters.AddWithValue("@Office", dgvRow.Cells["txtOffice"].Value == DBNull.Value ? "" : dgvRow.Cells["txtOffice"].Value.ToString());
+                    sqlCmd.Parameters.AddWithValue("@Age", Convert.ToInt32(dgvRow.Cells["txtAge"].Value == DBNull.Value ? "0" : dgvRow.Cells["txtAge"].Value.ToString()));
+
+                    sqlCmd.ExecuteNonQuery();
+                    dataGridViewEmployeeTOutput();
+                }
+            }
+        }
+
+        private void dataGridView1_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            if (dataGridView1.CurrentRow.Cells["txtEmployeeID"].Value != DBNull.Value)
+            {
+                if (MessageBox.Show("asd", "asdasd", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                    {
+                        sqlCon.Open();
+                        SqlCommand sqlCmd = new SqlCommand("EmployeeDelete", sqlCon);
+                        sqlCmd.CommandType = CommandType.StoredProcedure;
+                        sqlCmd.Parameters.AddWithValue("@EmployeeID", Convert.ToInt32(dataGridView1.CurrentRow.Cells["txtEmployeeID"].Value));
+                        sqlCmd.ExecuteNonQuery();
+                    }
+                }
+
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
